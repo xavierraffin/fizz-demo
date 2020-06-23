77def09861befd87adfda86448f4c58672d9a6a5
Vo <kevnvo@a483e743386a.ant.amazon.com>
2020-06-23 01:25:06 -0700

    Add locale to backend and fix lamada code.

--- amplify/backend/function/fizzauthCustomMessage/src/custom.js
+++ amplify/backend/function/fizzauthCustomMessage/src/custom.js
@@ -1,23 +1,28 @@
+
 exports.handler = (event, context, callback) => {
-    // Identify why was this function invoked
-    if (event.triggerSource === "CustomMessage_SignUp") {
-        let lang = event.request.userAttributes["custom:lang"]
-        console.log("Lang is " + lang);
+    if (event.userPoolId === "us-west-2_64AAJLQGz") { 
+        // Identify why was this function invoked
+        if (event.triggerSource === "CustomMessage_SignUp") {
+            let lang = event.request.userAttributes["custom:lang"]
+            console.log("Lang is " + lang);
 
 
-        // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
+            // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
 
-        if (lang == "FR") {
-            event.response.smsMessage = "Bienvenue à Fizz. Votre code de confirmation est " + event.request.codeParameter;
-            event.response.emailSubject = "Bienvenue à Fizz";
-            event.response.emailMessage = "Merci pour votre inscription. " + event.request.codeParameter + " est votre code de vérification";
-        } else {
-            event.response.smsMessage = "Welcome to Fizz. Your confirmation code is " + event.request.codeParameter;
-            event.response.emailSubject = "Welcome to Fizz";
-            event.response.emailMessage = "Thank you for signing up. " + event.request.codeParameter + " is your verification code";
+            if (lang == "FR") {
+                event.response.smsMessage = "Bienvenue à Fizz. Votre code de confirmation est " + event.request.codeParameter;
+                event.response.emailSubject = "Bienvenue à Fizz";
+                event.response.emailMessage = "Merci pour votre inscription. " + event.request.codeParameter + " est votre code de vérification";
+            } else {
+                event.response.smsMessage = "Welcome to Fizz. Your confirmation code is " + event.request.codeParameter;
+                event.response.emailSubject = "Welcome to Fizz";
+                event.response.emailMessage = "Welcome," + event.request.codeParameter + " is your verification code";
+            }
         }
+        // Create custom message for other events
     }
-    // Create custom message for other events
-  // Return to Amazon Cognito
-  callback(null, event);
+    // Customize messages for other user pools
+
+    // Return to Amazon Cognito
+    callback(null, event);
 };
