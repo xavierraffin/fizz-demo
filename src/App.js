c349a61d52bf406a7d0fbf92549c0c79cddc3cfc
Vo <kevnvo@a483e743386a.ant.amazon.com>
2020-06-28 12:24:19 -0700

    Did Translations and Successfully be able to send custom VIA email bylambda trigger set on cognitio. Also does not require custom userpool ID and would be able to any userpool now. I also raise a issue because the formfield cannot auto populate and be hidden due to a bug:  https://github.com/aws-amplify/amplify-js/issues/6168 . Also found a bunch of bugs with the forget password page as well, I am issuing a it on github now.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn, AmplifySignUp, AmplifyButton, AmplifySelectMfaType } from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn, AmplifySignUp, AmplifyButton, AmplifyForgotPassword, AmplifySelectMfaType } from '@aws-amplify/ui-react';
 import { I18n } from '@aws-amplify/core';
 import { strings } from './strings';
 
@@ -24,14 +24,45 @@ class App extends React.Component {
     super(props, context);
     this.state = { lang: "en" };
   }
-
-  toggleLang = () => {
+   //old toggle
+  /*toggleLang = () => {
+    console.log(this);
     if (this.state.lang === "en") {
       I18n.setLanguage("fr");
       this.setState({ lang: "fr" });
     } else {
       I18n.setLanguage("en");
       this.setState({ lang: "en" });
+    }
+  }*/
+// New toggle with more languages 
+  toggleLang = () => {
+    console.log(this);
+    switch(this.state.lang){
+      case "en": //English
+        I18n.setLanguage("fr");
+        this.setState({ lang: "fr" });
+        break;
+      case "fr":// French
+        I18n.setLanguage("es");
+        this.setState({ lang: "es" });
+        break;
+      case "es": //Spanish
+        I18n.setLanguage("de");
+        this.setState({ lang: "de" });
+        break;
+      case "de": //German
+        I18n.setLanguage("vn");
+        this.setState({ lang: "vn" });
+        break;
+      case "vi": //Vietnamese
+        I18n.setLanguage("en");
+        this.setState({ lang: "en" });
+        break;
+      default:
+        I18n.setLanguage("en");
+        this.setState({ lang: "en" });
+        break;
     }
   }
 
@@ -40,64 +71,101 @@ class App extends React.Component {
   render = () => (
     <div>
       <AmplifyButton onClick={this.toggleLang}>langue {this.state.lang}</AmplifyButton>
-      <AmplifyAuthenticator usernameAlias="email">
-        <AmplifySelectMfaType MFATypes={MFATypeOptions}></AmplifySelectMfaType>
-        <AmplifySignIn
-          usernameAlias="email"
-          headerText={I18n.get("signInHeader")}
-          slot="sign-in"
-          submitButtonText={I18n.get("signInButtonText")}
-          formFields={[
-            {
-              type: "email",
-              label: I18n.get("emailLabel"),
-              placeholder: I18n.get("emailPlaceHolder"),
-              required: true,
-            },
-            {
-              type: "password",
-              label: I18n.get("passwordLabel"),
-              placeholder: I18n.get("passwordPlaceHolder"),
-              required: true,
-            }
-          ]}></AmplifySignIn>
-        <AmplifySignUp
-          usernameAlias="email"
-          headerText={I18n.get("signUpHeader")}
-          slot="sign-up"
-          submitButtonText={I18n.get("signUpButtonText")}
-          formFields={[
-            {
-              type: "email",
-              label: I18n.get("emailLabel"),
-              placeholder: I18n.get("emailPlaceHolder"),
-              required: true,
-            },
-            {
-              type: "password",
-              label: I18n.get("passwordLabel"),
-              placeholder: I18n.get("passwordPlaceHolder"),
-              required: true,
-            },
-            {
-              type: "phone_number",
-              label: I18n.get("phoneNumberLabel"),
-              placeholder: I18n.get("phoneNumberPlaceHolder"),
-              required: false,
-            },
-            {
-              type: "hidden",
-              value: navigator.language,
-            }
-          ]}></AmplifySignUp>
-        <div>
-          {/* This is not really private, you should instead use the credential to load something dynamically from the backend */}
-            Private Content
-          <AmplifySignOut />
+      <div style={styles.container}>
+        <AmplifyAuthenticator usernameAlias="email">
+          <AmplifySelectMfaType MFATypes={MFATypeOptions}></AmplifySelectMfaType>
+          <AmplifyForgotPassword 
+            usernameAlias="email"
+            slot="forgot-password"
+            headerText= {I18n.get("resetYourPassword")}
+            submitButtonText= {I18n.get("sendCode")} //Bug?
+            formFields={[
+              {
+                type: "email",
+                label: I18n.get("emailLabel"),
+                placeholder: I18n.get("emailPlaceHolder"), //Bug?
+                required: true,
+              },
+            ]}></AmplifyForgotPassword>
+          <AmplifySignIn
+            usernameAlias="email"
+            headerText={I18n.get("signInHeader")}
+            //forgotYourPassword={I18n.get("forgotYourPassword")} No Support?
+            slot="sign-in"
+            submitButtonText={I18n.get("signInButtonText")}
+            formFields={[
+              {
+                type: "email",
+                label: I18n.get("emailLabel"),
+                placeholder: I18n.get("emailPlaceHolder"),
+                required: true,
+              },
+              {
+                type: "password",
+                label: I18n.get("passwordLabel"),
+                placeholder: I18n.get("passwordPlaceHolder"),
+                required: true,
+              }
+            ]}></AmplifySignIn>
+          <AmplifySignUp 
+            // Issue with auto populate and hidden: https://github.com/aws-amplify/amplify-js/issues/6168
+            usernameAlias="email"
+            headerText={I18n.get("signUpHeader")}
+            slot="sign-up"
+            signInText={I18n.get("signInText")}
+            haveAccountText={I18n.get("haveAccountText")}
+            submitButtonText={I18n.get("signUpButtonText")}
+            formFields={[
+              {
+                type: "email",
+                label: I18n.get("emailLabel"),
+                placeholder: I18n.get("emailPlaceHolder"),
+                required: true,
+              },
+              {
+                type: "password",
+                label: I18n.get("passwordLabel"),
+                placeholder: I18n.get("passwordPlaceHolder"),
+                required: true,
+              },
+              {
+                type: "phone_number",
+                label: I18n.get("phoneNumberLabel"),
+                placeholder: I18n.get("phoneNumberPlaceHolder"),
+                required: false,
+              },
+              {
+                type: "locale",
+                label: I18n.get("Locale"),
+                placeholder: "Locale-Code",
+                //value: navigator.language,
+                //Auto populate does not work for some reason there a bug with navigator.language
+                //For now we have to type in the string itself and cannot be hidden, Ampilfy team is working on this solution
+                required: true,
+              },
+              {
+                type: "custom:lang",
+                label: I18n.get("Lang"),
+                placeholder: "Lang-Code",
+                //value: this.state.lang,
+                //Auto populate does not work for some reason there a bug with navigator.language
+                //For now we have to type in the string itself and cannot be hidden, Ampilfy team is working on this solution
+                required: true,
+              },
+            ]}></AmplifySignUp>
+          <div>
+            {/* This is not really private, you should instead use the credential to load something dynamically from the backend */}
+              Private Content
+            <AmplifySignOut />
+          </div>
+        </AmplifyAuthenticator>
         </div>
-      </AmplifyAuthenticator>
     </div>
   );
+}
+
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 0 },
 }
 
 export default App;
